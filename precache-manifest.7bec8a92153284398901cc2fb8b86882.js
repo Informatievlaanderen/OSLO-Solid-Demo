5e04eae3ed9142471dc712e8175ca773cf6648dc
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-12-07 13:20:39 +0100

    Updates

---
+++ precache-manifest.7bec8a92153284398901cc2fb8b86882.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb40accb48ab344b5488aa6ebd9d61a",
+    "url": "/OSLO-Solid-Demo/index.html"
+  },
+  {
+    "revision": "ed5274a85efe39fade0e",
+    "url": "/OSLO-Solid-Demo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "11ce26d6b7d4195d02da",
+    "url": "/OSLO-Solid-Demo/static/css/main.f2c35b91.chunk.css"
+  },
+  {
+    "revision": "ed5274a85efe39fade0e",
+    "url": "/OSLO-Solid-Demo/static/js/2.84f4ca88.chunk.js"
+  },
+  {
+    "revision": "54dcc8b6e0cced9f2fe09050e90dcb02",
+    "url": "/OSLO-Solid-Demo/static/js/2.84f4ca88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ce26d6b7d4195d02da",
+    "url": "/OSLO-Solid-Demo/static/js/main.935ba755.chunk.js"
+  },
+  {
+    "revision": "0d8241c9e0b1f96bf25f",
+    "url": "/OSLO-Solid-Demo/static/js/runtime-main.55351eab.js"
+  },
+  {
+    "revision": "7fb32e6906cda533c2c7f2718fb33120",
+    "url": "/OSLO-Solid-Demo/static/media/idlablogo.7fb32e69.jpg"
+  },
+  {
+    "revision": "5969ee5b62bac1ecf711d2ef1e7d422d",
+    "url": "/OSLO-Solid-Demo/static/media/solid_background.5969ee5b.jpg"
+  }
+]);
